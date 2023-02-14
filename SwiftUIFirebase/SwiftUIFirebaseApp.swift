285ba3e0f29be58a5872ca6736f9acf27d769559
joneikholmkea <jone@kea.dk>
2023-02-14 09:46:04 +0100

    Initial Commit

---
+++ SwiftUIFirebase/SwiftUIFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseApp.swift
+//  SwiftUIFirebase
+//
+//  Created by Jon Eikholm on 14/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
